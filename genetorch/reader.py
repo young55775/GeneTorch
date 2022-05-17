2b0bce76676081ee54cd56ce3882a1e7f207c2cd
young55775 <91015935+young55775@users.noreply.github.com>
2022-05-17 14:27:02 +0800

    Update reader.py

--- genetorch/reader.py
+++ genetorch/reader.py
@@ -3,57 +3,63 @@ import io
 import os
 import shutil
 
+
 def search(df, col, kw):
     return df[col] == kw
 
 
 class readfile:
-    def __init__(self, path):
+    def __init__(self, path=None):
         self.path = path
         self.taglist = []
         self.names = []
         self.readfile()
+        self.co_data = pd.DataFrame()
+        self.result = pd.DataFrame()
+        self.suppressor_group = []
+        self.candidate = []
 
     def readfile(self):
-        files = os.listdir(self.path)
-        filelist = []
-        filename = []
+        if self.path != None:
+            files = os.listdir(self.path)
+            filelist = []
+            filename = []
 
-        # read vcf from file using pandas dataframe
-        def read_vcf(path_a):
-            with open(path_a, 'r') as f:
-                lines = [l for l in f if not l.startswith('##')]
-            return pd.read_csv(
-                io.StringIO(''.join(lines)),
-                dtype={'#CHROM': str, 'POS': int, 'ID': str, 'REF': str, 'ALT': str,
-                       'QUAL': str, 'FILTER': str, 'INFO': str},
-                sep='\t'
-            ).rename(columns={'#CHROM': 'CHROM'})
+            # read vcf from file using pandas dataframe
+            def read_vcf(path_a):
+                with open(path_a, 'r') as f:
+                    lines = [l for l in f if not l.startswith('##')]
+                return pd.read_csv(
+                    io.StringIO(''.join(lines)),
+                    dtype={'#CHROM': str, 'POS': int, 'ID': str, 'REF': str, 'ALT': str,
+                           'QUAL': str, 'FILTER': str, 'INFO': str},
+                    sep='\t'
+                ).rename(columns={'#CHROM': 'CHROM'})
 
-        for file in files:
-            if not os.path.isdir(file):
-                if os.path.basename(file).split('.')[1] == 'vcf':
-                    f = read_vcf(self.path + '/' + file)
-                    filelist.append(f)
-                    filename.append(os.path.basename(file).split('.')[0])
-        self.names = filename
+            for file in files:
+                if not os.path.isdir(file):
+                    if os.path.basename(file).split('.')[-1] == 'vcf':
+                        f = read_vcf(self.path + '/' + file)
+                        filelist.append(f)
+                        filename.append(os.path.basename(file).split('.')[0])
+            self.names = filename
 
-        def simp_file(raw_df):
-            b = []
-            for i, row in raw_df.iterrows():
-                b.append(row['INFO'].split('|'))
-            c = []
-            for i in range(len(b)):
-                c.append([b[i][3], b[i][4], b[i][1], b[i][9], b[i][10]])
-            genelist = pd.DataFrame(c, columns=['gene', 'ID', 'type', 'base', 'protein'])
-            return genelist
+            def simp_file(raw_df):
+                b = []
+                for i, row in raw_df.iterrows():
+                    b.append(row['INFO'].split('|'))
+                c = []
+                for i in range(len(b)):
+                    c.append([b[i][3], b[i][4], b[i][1], b[i][9], b[i][10]])
+                genelist = pd.DataFrame(c, columns=['gene', 'ID', 'type', 'base', 'protein'])
+                return genelist
 
-        simp_list = []
-        for i in range(len(filelist)):
-            simp_list.append(simp_file(filelist[i]))
-        self.taglist = simp_list
-        for i in range(len(self.taglist)):
-            self.taglist[i].insert(loc=len(self.taglist[i].columns), column='tag', value=filename[i])
+            simp_list = []
+            for i in range(len(filelist)):
+                simp_list.append(simp_file(filelist[i]))
+            self.taglist = simp_list
+            for i in range(len(self.taglist)):
+                self.taglist[i].insert(loc=len(self.taglist[i].columns), column='tag', value=filename[i])
 
 
 class getfile:
@@ -64,6 +70,8 @@ class getfile:
         self.taglist = []
         self.names = []
         self.splitfile()
+        self.suppressor_group = []
+        self.candidate = []
 
     def splitfile(self):
         os.mkdir(self.temp)
@@ -125,14 +133,31 @@ class getfile:
             self.taglist[i].insert(loc=len(self.taglist[i].columns), column='tag', value=self.names[i])
 
 
-
-def get_impact(taglist):
-    for i in range(len(taglist)):
-        n = taglist[i]
-        indp = search(n,'protein','')
+def get_impact(a):
+    newtaglist = []
+    for i in range(len(a.taglist)):
+        n = a.taglist[i]
+        lista = []
+        listb = []
+        splice_donor = n[n['type'] == 'splice_donor_variant&intron_variant']['protein'].tolist()
+        splice_acceptor = n[n['type'] == 'splice_acceptor_variant&intron_variant']['protein'].tolist()
+        for i in range(len(splice_donor)):
+            lista.append('X_donor')
+        for i in range(len(splice_acceptor)):
+            listb.append('X_acceptor')
+        c = pd.DataFrame(n[n['type'] == 'splice_donor_variant&intron_variant'])
+        d = pd.DataFrame(n[n['type'] == 'splice_acceptor_variant&intron_variant'])
+        c['protein'] = lista
+        d['protein'] = listb
+        n = pd.concat([n, c, d])
+        indp = search(n, 'protein', '')
+        indq = search(n, 'protein', 'nan')
         ind = search(n, 'type', 'synonymous_variant')
         m = n.loc[ind, :]
-        a = n.loc[indp, :]
-        n = pd.concat([m, n]).drop_duplicates(keep=False)
-        taglist[i] = pd.concat([n,a]).drop_duplicates(keep=False)
-    return taglist
+        b = n.loc[indp, :]
+        e = n.loc[indq, :]
+        n = pd.concat([m, n, e]).drop_duplicates(keep=False)
+        total = pd.concat([n, b]).drop_duplicates(keep=False)
+        newtaglist.append(total)
+    a.taglist = newtaglist
+    return a.taglist
